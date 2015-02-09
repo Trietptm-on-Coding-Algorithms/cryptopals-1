dea7786fc0e5607a2b23a5775e9497119740090e
Adam Przybyszewski <spdc88@gmail.com>
2015-02-09 17:04:45 -0500

    Added solution for s1e02 which xors two blocks of data together

--- sets/1/src/s1c02.c
+++ sets/1/src/s1c02.c
@@ -7,11 +7,24 @@
 #import "include/s1c02.h"
 
 int main(int argc, char **argv) {
-    if(argc != 3){
+    if( (argc != 3) || (strlen(argv[1]) != strlen(argv[2])) ){
         printf("Error: Please provide 2 equal-length hex strings.\n");
         return -1;
     }
 
+    unsigned char *dataBlockOne = loadHexStringToMemory(argv[1]);
+    unsigned char *dataBlockTwo = loadHexStringToMemory(argv[2]);
+    if(!dataBlockOne || !dataBlockTwo){
+        printf("Error: Unable to load data blocks into memory.\n");
+        return -1;
+    }
+
+    char *result = xorDataBlocks(dataBlockOne, dataBlockTwo, strlen(argv[1]) / 2);
+    printf("Result: %s\n", result);
+
+    free(dataBlockOne);
+    free(dataBlockTwo);
+    free(result);
 }
 
 
