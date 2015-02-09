6b29ab0ab2401d38024479e0269b6f86b43351ee
Adam Przybyszewski <spdc88@gmail.com>
2015-02-09 16:18:34 -0500

    Created file to start 2nd challenge of set 1

---
+++ sets/1/src/s1c02.c
@@ -0,0 +1,18 @@
+/**
+  * Matasano Crypto Challenge Solution
+  * Set: 01, Challenge 02: Fixed XOR
+  *
+  * Coded by rot0x1a, 09 Feb 2015
+  */
+#import "include/s1c02.h"
+
+int main(int argc, char **argv) {
+    if(argc != 3){
+        printf("Error: Please provide 2 equal-length hex strings.\n");
+        return -1;
+    }
+
+    printf("%s\n", hexToBase64(argv[1]));
+}
+
+
