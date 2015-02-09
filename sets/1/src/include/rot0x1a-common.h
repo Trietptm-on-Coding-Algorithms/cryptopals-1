1978d524237a41a653cd2f25f477a10dd37ec55a
Adam Przybyszewski <spdc88@gmail.com>
2015-02-09 16:38:15 -0500

    Added documentation to common code prototypes in common header

--- sets/1/src/include/rot0x1a-common.h
+++ sets/1/src/include/rot0x1a-common.h
@@ -7,7 +7,26 @@
 
 static const char BASE64_ENCODING_VALUES[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
+/**
+  * Provided a string of hex characters, allocate a chunk of memory and load
+  * the hex characters into the chunk as raw data.
+  *
+  * @param hexString The hexadecimal characters load into memory.
+  *
+  * @return The memory address we loaded the hex data into.
+  */
 unsigned char *loadHexStringToMemory(char *hexString);
+
+
+/**
+  * Given a pointer to memory, encode the specified amount of data as base64 
+  * and return the result as a string.
+  *
+  * @param data The location of the data to be encoded.
+  * @param dataLength The number of bytes to encode.
+  *
+  * @return The base64 string representation of the data.
+  */
 char *base64Encode(unsigned char *data, int dataLength);
 
 #endif
