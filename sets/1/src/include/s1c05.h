5a661ce7e6109066178612c1d3e2105c47527dd0
rot0x1a <spdc88@gmail.com>
2015-02-10 16:26:44 -0800

    Added s1c05 to Makefile and added skeleton code for the solution

---
+++ sets/1/src/include/s1c05.h
@@ -0,0 +1,6 @@
+#ifndef S1C05_H
+#define S1C05_H
+
+#include "rot0x1a-common.h"
+
+#endif
---
+++ sets/1/src/s1c05.c
@@ -0,0 +1,11 @@
+/**
+  * Matasano Crypto Challenge Solution
+  * Set: 01, Challenge 05: Implement repeating-key XOR
+  *
+  * Coded by rot0x1a, 10 Feb 2015
+  */
+#include "include/s1c05.h"
+
+int main(int argc, char **argv) {
+
+}
