4563ff919151f407d9208989a82523d4b782d96f
rot0x1a <spdc88@gmail.com>
2015-02-11 14:51:59 -0800

    Added expected inputs and values to cryptopals header. displays the output vs the expected for comparison

--- cryptopals/src/cryptopals.c
+++ cryptopals/src/cryptopals.c
@@ -1,35 +1,45 @@
 #include "cryptopals.h"
 
+
 int main(int argc, char **argv) {
 
-    printf("-----------------------------------------------------\n");
-    printf("|########## Matasano's Crypto Challenges! ##########|\n");
-    printf("-----------------------------------------------------\n");
+    printf("\n====================================================================================\n");
+    printf("                           Matasano's Crypto Challenges Set 1                           ");
+    printf("\n====================================================================================\n");
 
-    printf("[Set 01 :: Challenge 01]\n");
-    char *resultOne = solveSet1Challenge01(NULL);
+    printf(" ------------------------ \n");
+    printf("| Set 01 ::Challenge 01] |\n");
+    printf(" ------------------------ \n");
+    char *resultOne = solveSet1Challenge01(SET_1_CHALLENGE_1_INPUT_1);
     if(resultOne){
-        printf("Result: %s\n\n", resultOne);
+        printf("Expected: %s\n", SET_1_CHALLENGE_1_EXPECTED_OUTPUT);
+        printf("Result:   %s\n\n", resultOne);
         free(resultOne);
     } else {
         printf("Failed.\n\n");
     }
 
-    printf("[Set 01 :: Challenge 02]\n");
-    char *resultTwo = solveSet1Challenge02(NULL, NULL);
+    printf(" ------------------------ \n");
+    printf("| Set 01 :: Challenge 02 |\n");
+    printf(" ------------------------ \n");
+    char *resultTwo = solveSet1Challenge02(SET_1_CHALLENGE_2_INPUT_1, SET_1_CHALLENGE_2_INPUT_2);
     if(resultTwo){
-        printf("Result: %s\n\n", resultTwo);
+        printf("Expected: %s\n", SET_1_CHALLENGE_2_EXPECTED_OUTPUT);
+        printf("Result:   %s\n\n", resultTwo);
         free(resultTwo);
     } else {
         printf("Failed.\n\n");
     }
 
-    printf("[Set 01 :: Challenge 03]\n");
-    xorDecryptedMessage *resultThree = solveSet1Challenge03(NULL);
+    printf(" ------------------------ \n");
+    printf("| Set 01 :: Challenge 03 |\n");
+    printf(" ------------------------ \n");
+    xorDecryptedMessage *resultThree = solveSet1Challenge03(SET_1_CHALLENGE_3_INPUT_1);
     if(resultThree){
-        printf("Key: %s\n", resultThree->key);
-        printf("Score: %d\n", resultThree->numberOfMatches);
-        printf("Message: %s\n\n", resultThree->message);
+        printf("Expected Key: %s\n", SET_1_CHALLENGE_3_EXPECTED_KEY);
+        printf("Result Key:   %s\n", resultThree->key);
+        printf("Expected Message: %s\n", SET_1_CHALLENGE_3_EXPECTED_MESSAGE);
+        printf("Result Message:   %s\n\n", resultThree->message);
         free(resultThree->key);
         free(resultThree->message);
         free(resultThree);
@@ -37,14 +47,15 @@ int main(int argc, char **argv) {
         printf("Failed.\n\n");
     }
 
-    printf("[Set 01 :: Challenge 04]\n");
-    // I broke it
-    // xorDecryptedMessage *resultFour = solveSet1Challenge04(NULL);
-    xorDecryptedMessage *resultFour = NULL;
+    printf(" ------------------------ \n");
+    printf("| Set 01 :: Challenge 04 |\n");
+    printf(" ------------------------ \n");
+    xorDecryptedMessage *resultFour = solveSet1Challenge04(SET_1_CHALLENGE_4_INPUT_1);
     if(resultFour){
-        printf("Key: %s\n", resultFour->key);
-        printf("Score: %d\n", resultFour->numberOfMatches);
-        printf("Message: %s\n\n", resultFour->message);
+        printf("Expected Key: %s\n", SET_1_CHALLENGE_4_EXPECTED_KEY);
+        printf("Result Key:   %s\n", resultFour->key);
+        printf("Expected Message: %s\n", SET_1_CHALLENGE_4_EXPECTED_MESSAGE);
+        printf("Result Message:   %s\n\n", resultFour->message);
         free(resultFour->key);
         free(resultFour->message);
         free(resultFour);
@@ -52,10 +63,13 @@ int main(int argc, char **argv) {
         printf("Failed.\n\n");
     }
 
-    printf("[Set 01 :: Challenge 05]\n");
-    char *resultFive = solveSet1Challenge05(NULL, NULL);
+    printf(" ------------------------ \n");
+    printf("| Set 01 :: Challenge 05 |\n");
+    printf(" ------------------------ \n");
+    char *resultFive = solveSet1Challenge05(SET_1_CHALLENGE_5_INPUT_1, SET_1_CHALLENGE_5_INPUT_2);
     if(resultFive){
-        printf("Result: %s\n\n", resultFive);
+        printf("Expected: %s\n", SET_1_CHALLENGE_5_EXPECTED_OUTPUT);
+        printf("Result:   %s\n\n", resultFive);
         free(resultFive);
     } else {
         printf("Failed.\n\n");
